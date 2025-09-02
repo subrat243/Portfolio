0a7bf829cb247186e9062a6d20c02a12a000c06d
subrat243 <subratsamantaray43@gmail.com>
2025-09-02 21:11:47 +0530

    Update

--- components/main/star-background.tsx
+++ components/main/star-background.tsx
@@ -41,7 +41,7 @@ export const StarBackground = (props: PointsProps) => {
 };
 
 export const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 -z-10">
+  <div className="w-full h-auto fixed inset-0 -z-70">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
